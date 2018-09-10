29581841ab81eec25661fe3fecd92b8db2463a25
Larry Pohto <Lp3_2@Hotmail.com>
2018-09-10 11:31:42 -0400

    Same as previous commit

--- backend/src/main/java/lp3/show_program/ShowProgramApplication.java
+++ backend/src/main/java/lp3/show_program/ShowProgramApplication.java
@@ -1,19 +1,12 @@
 package lp3.show_program;
 
-import java.lang.reflect.Method;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-
 
 @SpringBootApplication
 public class ShowProgramApplication {
