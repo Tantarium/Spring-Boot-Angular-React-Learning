b17e1cf8d5faf99f73ee8f4d1bf5aae806e05b59
Larry Pohto <Lp3_2@Hotmail.com>
2018-09-10 11:29:12 -0400

    Removed unused imports and commented out code

--- backend/src/main/java/lp3/show_program/ShowProgramApplication.java
+++ backend/src/main/java/lp3/show_program/ShowProgramApplication.java
@@ -35,33 +35,4 @@ public class ShowProgramApplication {
 		};
 	}
 
-//	@Bean
-//	public WebMvcRegistrations webMvcRegistrationsHandlerMapping() {
-//		ShowProgramApplication application = this;
-//		return new WebMvcRegistrations() {
-//			@Override
-//			public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-//				return new RequestMappingHandlerMapping() {
-//
-//					@Override
-//					protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
-//						Class<?> beanType = method.getDeclaringClass();
-//						RestController restApiController = beanType.getAnnotation(RestController.class);
-//						if (restApiController != null) {
-//							PatternsRequestCondition apiPattern = new PatternsRequestCondition(application.restApiBasePath)
-//									.combine(mapping.getPatternsCondition());
-//
-//							mapping = new RequestMappingInfo(mapping.getName(), apiPattern,
-//									mapping.getMethodsCondition(), mapping.getParamsCondition(),
-//									mapping.getHeadersCondition(), mapping.getConsumesCondition(),
-//									mapping.getProducesCondition(), mapping.getCustomCondition());
-//						}
-//
-//						super.registerHandlerMethod(handler, method, mapping);
-//					}
-//				};
-//			}
-//		};
-//	}
-
 }
