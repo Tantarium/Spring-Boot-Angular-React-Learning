393bba4a445ddcf7b431d70ec9e04ecb0b4e58f1
Larry Pohto <Lp3_2@Hotmail.com>
2018-09-21 12:03:18 -0400

    Added basic spring security to the back end of the project and got the rest endpoints being able to be triggered in Postman

--- backend/src/main/java/lp3/show_program/ShowProgramApplication.java
+++ backend/src/main/java/lp3/show_program/ShowProgramApplication.java
@@ -4,6 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -26,6 +33,32 @@ public class ShowProgramApplication {
 				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
 			}
 		};
+	}
+
+	@EnableWebSecurity
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+					.antMatchers("/action/**")
+					.access("isFullyAuthenticated()")
+					.and()
+					.formLogin()
+					.and()
+					.csrf().disable();
+		}
+
+		@Bean
+		@Override
+		public UserDetailsService userDetailsService() {
+			UserDetails user = User.withDefaultPasswordEncoder()
+					.username("user")
+					.password("password")
+					.roles("USER")
+					.build();
+
+			return new InMemoryUserDetailsManager(user);
+		}
 	}
 
 }
